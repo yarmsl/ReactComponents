6b0a6036a8e541c6d2bd2f09c9b6ac61d4fb8020
Yaroslav <slideryo@gmail.com>
2021-04-12 08:01:04 +0500

    1.0004

--- src/Main.js
+++ src/Main.js
@@ -14,21 +14,27 @@ export default function Main() {
 //    }
 // }
 
+const [nav, setNav] = useState({});
+
+const handleChange = e => {
+   e.preventDefault();
+   setNav({category: e.target.value});
+};
+
 let arr = [];
+
 function categories(obj) {
 for (let key in obj.category) {
    arr = arr.concat([{id: obj.category[key].id, name: obj.category[key].name}]);
    } 
    arr = (
-   <select> 
-      <option></option>
+   <select  onChange={(e) => {handleChange(e)}}> 
       {arr.map(item => {
      return (
      <option key={item.id} value={item.name}>{item.name}</option>
      )})} 
    </select>
 )
-
 }
 
 categories(NavigateObject);
@@ -44,12 +50,33 @@ function sort(obj) {
       }
    }
 }
-sort(navObj);
+
+
+// let arr2 = [
+// {id: 1, name: 'Acura'},
+// {id: 2, name: 'Lexus'},
+// {id: 3, name: 'Infinity'},
+// {id: 4, name: 'Mazda'},
+// {id: 5, name: 'Subaru'}
+// ];
+
+// const output = arr2.forEach(item => {
+//    console.log(item.name)
+// });
+
+const nO = [navObj];
+
+let iip = nO.map((item, i) => {
+  i = Object.keys(item);
+  return ( <div>{item.i}</div>)
+})
+
    return (
       <main className="main">
 
    {arr}
-
+   <div>Выбрано: {nav.category}</div>
+   {iip}
       </main>
    )
 }
