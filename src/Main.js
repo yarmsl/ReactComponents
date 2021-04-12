a4f00e48e2b99c93e83063485d2c1c8578851951
Yaroslav <slideryo@gmail.com>
2021-04-12 14:00:16 +0500

    12.04.2021

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import NavigateObject from './json/navigateObject.json';
 
+
 export default function Main() {
 
 
@@ -27,6 +28,7 @@ export default function Main() {
       for (let key in obj.category) {
          arr = arr.concat([{ id: obj.category[key].id, name: obj.category[key].name }]);
       }
+      console.log(arr);
       arr = (
          <select onChange={(e) => { handleChange(e) }}>
             {arr.map(item => {
@@ -36,48 +38,49 @@ export default function Main() {
             })}
          </select>
       )
+      console.log(arr);
    }
 
    categories(NavigateObject);
 
    const navObj = NavigateObject.category;
 
-   function sort(obj) {
-      for (let key in obj) {
-         if (typeof (obj[key].subCategories) === 'object') {
-            for (let i in obj[key].subCategories) {
-               console.log(obj[key].subCategories[i])
-            }
-         }
-      }
-   }
+   // function sort(obj) {
+   //    for (let key in obj) {
+   //       if (typeof (obj[key].subCategories) === 'object') {
+   //          for (let i in obj[key].subCategories) {
+   //             console.log(obj[key].subCategories[i])
+   //          }
+   //       }
+   //    }
+   // }
 
 
-   let arr2 = [
-      { id: 1, name: 'Acura' },
-      { id: 2, name: 'Lexus' },
-      { id: 3, name: 'Infinity' },
-      { id: 4, name: 'Mazda' },
-      { id: 5, name: 'Subaru' }
-   ];
+   // let arr2 = [
+   //    { id: 1, name: 'Acura' },
+   //    { id: 2, name: 'Lexus' },
+   //    { id: 3, name: 'Infinity' },
+   //    { id: 4, name: 'Mazda' },
+   //    { id: 5, name: 'Subaru' }
+   // ];
 
-   const output = arr2.forEach(item => {
-      console.log(item.name)
-   });
+   // const output = arr2.forEach(item => {
+   //    console.log(item.name)
+   // });
 
-   const nO = [navObj];
+   // const nO = [navObj];
 
-   let iip = nO.map((item, i) => {
-      i = Object.keys(item);
-      return (<div>{item.i}</div>)
-   })
+   // let iip = nO.map((item, i) => {
+   //    i = Object.keys(item);
+   //    return (<div>{item.i}</div>)
+   // })
 
    return (
       <main className="main">
 
          {arr}
          <div>Выбрано: {nav.category}</div>
-         {iip}
+         {/* {iip} */}
       </main>
    )
 }
